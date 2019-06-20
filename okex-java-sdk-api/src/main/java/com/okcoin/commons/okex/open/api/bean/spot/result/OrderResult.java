d0ac8c1a0bceceac249d433f1940e363bef67b29
廖师兄 <null@163.com>
2019-06-20 14:20:10 +0800

    下单结果新增字段error_code、error_message 见 https://www.okex.me/docs/zh/#spot-orders

--- okex-java-sdk-api/src/main/java/com/okcoin/commons/okex/open/api/bean/spot/result/OrderResult.java
+++ okex-java-sdk-api/src/main/java/com/okcoin/commons/okex/open/api/bean/spot/result/OrderResult.java
@@ -1,10 +1,22 @@
 package com.okcoin.commons.okex.open.api.bean.spot.result;
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * 下单结果
+ * https://www.okex.me/docs/zh/#spot-orders
+ */
 public class OrderResult {
 
     private boolean result;
     private Long order_id;
     private String client_oid;
+
+    @SerializedName("error_code")
+    private String errorCode;
+
+    @SerializedName("error_message")
+    private String errorMessage;
 
     public String getClient_oid() {
         return this.client_oid;
@@ -28,5 +40,21 @@ public class OrderResult {
 
     public void setOrder_id(final Long order_id) {
         this.order_id = order_id;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
